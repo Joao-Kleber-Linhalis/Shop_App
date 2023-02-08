91e47c7ab5a56620bc9f28dc81d4e94302ec2ac3
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-02-08 19:20:15 -0300

    Favorite Filter

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -5,10 +5,12 @@ import 'package:shop/util/app_routes.dart';
 import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -20,12 +22,15 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           backgroundColor: Colors.black54,
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavorite();
-            },
-            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              onPressed: () {
+                product.toggleFavorite();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            ),
           ),
           trailing: IconButton(
             onPressed: () {},
--- lib/screens/product_overviews_screen.dart
+++ lib/screens/product_overviews_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_lista.dart';
 import '../components/productGrid.dart';
+
+
+ enum FilterOptions{
+  favorite,
+  all,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   ProductOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favorito'),
+                value: FilterOptions.favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue){
+              if(selectedValue == FilterOptions.favorite){
+                provider.showFavoriteOnly();
+              }
+              else{
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: ProductGrid(),
     );
