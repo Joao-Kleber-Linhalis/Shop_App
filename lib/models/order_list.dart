3368eb2ebf7dce179fd815cfddc506ebfe20bc6d
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-01-26 10:16:12 -0300

    Order per User

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -9,11 +9,13 @@ import 'package:shop/util/constants.dart';
 
 class OrderList with ChangeNotifier {
   final String _token;
+  final String _userId;
 
   List<Order> _items = [];
 
   OrderList([
     this._token = '',
+    this._userId = '',
     this._items = const [],
   ]);
 
@@ -29,7 +31,7 @@ class OrderList with ChangeNotifier {
     List<Order> items = [];
 
     final response = await http
-        .get(Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'));
+        .get(Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'));
     if (response.body == 'null') return; //Não tem pedidos na lista
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((orderId, orderData) {
@@ -58,7 +60,7 @@ class OrderList with ChangeNotifier {
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('${Constants.ORDER_BASE_URL}.json?auth=$_token'),
+      Uri.parse('${Constants.ORDER_BASE_URL}/$_userId.json?auth=$_token'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
