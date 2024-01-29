ddf92f22ac669098556a850643b7d7431cdad3da
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-01-29 15:17:19 -0300

    AnimatedBuilder

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -46,7 +46,7 @@ class _AuthFormState extends State<AuthForm>
       curve: Curves.linear,
     ));
 
-    _heightAnimation?.addListener(() => setState(() {}));
+    //_heightAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -125,11 +125,15 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        //height: _isLogin() ? 310 : 400,
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-        width: deviceSize.width * 0.75,
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (ctx, childForm) => Container(
+          padding: const EdgeInsets.all(16),
+          //height: _isLogin() ? 310 : 400,
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+          width: deviceSize.width * 0.75,
+          child: childForm,
+        ),
         child: Form(
           key: _formKey,
           child: Column(
