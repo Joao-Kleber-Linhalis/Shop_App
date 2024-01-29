45cf7f9a704e4fbe9a8974183c8964bc6b3e16a1
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-01-29 17:12:12 -0300

    Animation for Order

--- lib/components/order.dart
+++ lib/components/order.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
@@ -14,30 +15,34 @@ class _OrderWidgetState extends State<OrderWidget> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+    final itemsHeight = widget.order.products.length * 30.0 + 5;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              height: _expanded ? itemsHeight : 0,
               padding: const EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: widget.order.products.length * 30 + 5,
               child: ListView(
                 children: widget.order.products.map(
                   (product) {
@@ -51,14 +56,18 @@ class _OrderWidgetState extends State<OrderWidget> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Text("${product.quantity}x R\$ ${product.price}", style: TextStyle(fontSize: 18,color: Colors.grey),),
+                        Text(
+                          "${product.quantity}x R\$ ${product.price}",
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
                       ],
                     );
                   },
                 ).toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
