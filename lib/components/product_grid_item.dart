56c84bc21726b15150c7252348c4895bd9274e50
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-01-29 16:31:42 -0300

    Hero Widget

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -64,10 +64,13 @@ class ProductGridItem extends StatelessWidget {
           ),
         ),
         child: GestureDetector(
-          child: FadeInImage(
-            placeholder: AssetImage("assets/images/product-placeholder.png"),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage("assets/images/product-placeholder.png"),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
           onTap: () {
             Navigator.of(context).pushNamed(
